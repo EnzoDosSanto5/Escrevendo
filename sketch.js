784723c2defc164e83bd039f14e58d1b895f8138
EnzoDosSanto5 <00001082340352sp@al.educacao.sp.gov.br>
2024-05-29 09:51:45 -0300

    adicionando arquevos js ecss

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(600, 600);
+     background("#2196F3");
+  }
+  
+  function draw() { 
+      stroke("blue")
+    fill("red")
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 30, 35);
+    }
+    
